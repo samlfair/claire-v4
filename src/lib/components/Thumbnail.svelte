2cff36d7839fddfd09f58b94194cb9d0647fd8fd
Sam Littlefair <sam.littlefair@prismic.io>
2022-05-28 01:57:21 +0100

    Mockup

---
+++ src/lib/components/Thumbnail.svelte
@@ -0,0 +1,62 @@
+<script>
+  export let reverse
+</script>
+
+<article>
+  <div class="padding"  class:reverse>
+
+    <div class="image">
+      <div class="picture"></div>
+    </div>
+    <div class="text">
+      <h2>Lorem Ipsum</h2>
+      <p>Dolor sit amet.</p>
+    </div>
+  </div>
+</article>
+
+<style>
+  article {
+    margin: min(10vw, 4rem) 0;
+  }
+
+  .padding {
+    align-items: center;
+    gap: 4vw;
+    margin: -1rem;
+    padding: 1rem;
+    display: flex;
+    transition: all 0.6s;
+  }
+  
+  .padding.reverse {
+    flex-direction: row-reverse;
+  }
+
+  .padding:hover {
+    box-shadow: 
+      0px 4px 20px 14px rgba(0,0,0,0.08), 
+      0px 2px 5px 1px rgba(0,0,0,0.1);
+      transform: translateY(-1px)
+  }
+
+  .text {
+    flex: 2;
+  }
+
+  .picture {
+    padding-top: clamp(130px, 20vw, 180px);
+    background: grey;
+  }
+
+  .image {
+    /* aspect-ratio: 1.8; */
+    flex: 1.2vw;
+    align-self: flex-start;
+  }
+
+  h2 {
+    margin-bottom:  0.5rem;
+    font-size: 1.4rem;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import logo from "./logo.png"
+  import Thumbnail from "../lib/components/Thumbnail.svelte";
+</script>
+
+<main>
+  <nav>
+    <img src={logo} alt="logo">
+    <p>Jewellery · Design</p>
+  </nav>
+  <h2>Dolor sit</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, laboriosam modi! Est in recusandae porro! Fugiat assumenda, labore hic at consectetur eius accusamus ipsa nam quaerat veniam tenetur eos voluptatibus!
+  </p>
+  <Thumbnail />
+  <Thumbnail reverse />
+  <Thumbnail />
+</main>
+
+<style>
+  :global(html, body) {
+    margin: 0;
+    body: 0;
+  }
+
+  img {
+    max-width: clamp(200px, 30vw, 400px);
+    margin: 1.5rem auto;
+    display: block;
+  }
+
+  :global(body) {
+    min-height: 100vh;
+  }
+
+  main {
+    width: 100%;
+    max-width: min(calc(100vw - 5rem), 800px);
+    margin: auto;
+  }
+
+  nav {
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin: 3rem auto;
+    text-align: center;
+    color: rgba(0,0,0,0.6)
+  }
+  
+  :global(h2) {
+    font-family: "Americana";
+    font-size: 2rem;
+  }
+
+  :global(p) {
+    font-family: "Sweet Sans Pro";
+    margin: 0;
+  }
+</style>
